7273f0935dae3402e1b8b390d737d2a4227b92f4
Hank Ke <59212751+hke-cheng@users.noreply.github.com>
2020-05-16 23:47:41 -0700

    Init project

---
+++ src/Palette.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colors">
+          {/* bunch of boxes */}
+        </div>
+        {/*  footer eventually*/}
+      </div>
+    );
+  }
+}
+
+export default Palette;

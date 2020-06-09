aac63824ff4b259dfde1bd08606809e8eeea9402
Hank Ke <59212751+hke-cheng@users.noreply.github.com>
2020-06-08 22:51:16 -0700

    Add Color Chrome Picker and Buttons to drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,9 +11,11 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Button from '@material-ui/core/Button';
+import { ChromePicker} from 'react-color';
 
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -93,7 +95,7 @@ export default function NewPaletteForm() {
           [classes.appBarShift]: open,
         })}
       >
-        //
+
         <Toolbar>
           <IconButton
             color="inherit"
@@ -110,7 +112,7 @@ export default function NewPaletteForm() {
           </Typography>
         </Toolbar>
       </AppBar>
-      //
+
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -126,10 +128,23 @@ export default function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
-        <Divider />
+        <Typography variant='h4'>Design Palette</Typography>
+        <div>
+          <Button variant="contained" color="secondary"> Clear Palette</Button>
+          <Button variant="contained" color="primary"> Random Color</Button>
+        </div>
+
+        <ChromePicker
+          color="purple"
+          onChangeComplete={(newColor) => console.log(newColor)}
+        />
+  
+
+        <Button
+          variant="container"
+          color="primary">Add Color</Button>
 
       </Drawer>
-      //
 
       <main
         className={clsx(classes.content, {
